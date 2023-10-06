5f5446db2b588f87d48d3c15fcbe840633e4c75d
Saharnaz1 <sa_rdh@yahoo.com>
2023-10-06 10:23:31 -0400

    created SwapNumbers_Sahar.java

--- src/week2/SwapNumbers_Sahar.java
+++ src/week2/SwapNumbers_Sahar.java
@@ -1,4 +1,24 @@
 package week2;
 
 public class SwapNumbers_Sahar {
+    public static void main(String[] args) {
+
+        int x = 7;
+        int y= 10;
+
+        x= x+y;
+        y= x-y;
+        x= x-y;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+
+        /*
+        x= 5+ 10 result 15
+        y= 15-10 result 5
+        x= 15-5 result 10
+
+         */
+
+    }
 }
