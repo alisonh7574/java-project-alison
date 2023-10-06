d3b1f332df76eb3b1e019ea832eb26db328bbf00
Saharnaz1 <sa_rdh@yahoo.com>
2023-10-06 10:24:14 -0400

    create ConsecutiveNumbers_Sahar.java

--- src/week2/ConsecutiveNumbers_Sahar.java
+++ src/week2/ConsecutiveNumbers_Sahar.java
@@ -1,4 +1,27 @@
 package week2;
 
 public class ConsecutiveNumbers_Sahar {
+    public static void main(String[] args) {
+
+        int n = 100;
+        for (int i = 1; i <= n; i++) {
+            String result = "";
+
+            if (i % 2 == 0) {
+                result += "Codility";
+            }
+
+            if (i % 3 == 0) {
+                result += "Test";
+            }
+
+            if (i % 5 == 0) {
+                result += "Coders";
+            }
+
+            System.out.println(result.isEmpty() ? i : result);
+
+
+        }
+    }
 }
