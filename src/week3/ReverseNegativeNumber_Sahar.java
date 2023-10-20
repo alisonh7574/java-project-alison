751cdca12930bb34ded5f6a70ac94ee4d5502bb0
Saharnaz1 <sa_rdh@yahoo.com>
2023-10-19 21:48:52 -0400

    update sahar week3 PrimeNumber and ReverseNegative

--- src/week3/ReverseNegativeNumber_Sahar.java
+++ src/week3/ReverseNegativeNumber_Sahar.java
@@ -3,17 +3,28 @@ package week3;
 public class ReverseNegativeNumber_Sahar {
     public static void main(String[] args) {
 
-        System.out.println(reverseNegative(25));
+        int number = -123;
+        reverseNegative(number);
     }
 
-    public static int reverseNegative(int number) {
-        if (number < 0) {
-            return -number;
+    public static void reverseNegative(int number) {
+        String result = "";
+        String s = String.valueOf(number);
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result += s.charAt(i);
+
         }
-        return number;
+
+        String s1 = "-" + result.substring(0, result.length() - 1);
+
+        System.out.println(s1);
     }
 
 
-}
+    }
+
+
+
 // 2. Numbers -- Reverse negative number
 //  Write a return method that can reverse negative number and return it as int
