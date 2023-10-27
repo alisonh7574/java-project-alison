efc8dcab1106bd04d8e35145f443f2257a4c82fa
Saharnaz1 <sa_rdh@yahoo.com>
2023-10-27 15:06:39 -0400

    add Reverse_Sahar

--- src/week5/Reverse_Sahar.java
+++ src/week5/Reverse_Sahar.java
@@ -1,6 +1,24 @@
 package week5;
 
 public class Reverse_Sahar {
+    public static void main(String[] args) {
+        reverseWord("Anna");
+    }
+    public static void reverseWord(String word){
+        String reverse = "";
+        for (int i = word.length()-1; i >=0 ; i--) {
+            reverse += word.charAt(i);
+        }
+        System.out.println(reverse);
+        if (word.equalsIgnoreCase(reverse)){
+            System.out.println("Word is palindrome");
+        }else{
+            System.out.println("Word is not palindrome");
+        }
+    }
+
+
+
 
 
 }
