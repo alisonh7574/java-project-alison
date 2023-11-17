68cb4d0ddfe97e208d4835a620c5a11ec7ef6c26
Saharnaz1 <sa_rdh@yahoo.com>
2023-11-17 14:03:22 -0500

    Add week 7 sahar classes

--- src/week7/ArrayMin_Sahar.java
+++ src/week7/ArrayMin_Sahar.java
@@ -1,4 +1,27 @@
 package week7;
 
 public class ArrayMin_Sahar {
+
+    public static void main(String[] args) {
+
+
+        int[] array = {15, -2, 11, 25, 13, 4, 9};
+        System.out.println(minNumber(array));
+
+    }
+
+    public static int minNumber(int[] array) {
+
+        int min = Integer.MAX_VALUE; // Initialize min with the largest possible value
+        //    int min = array[0]  another option, use first index as default then move to the next one
+
+        for (int each : array) { // go through each number, each represents each number
+            if (each < min) { // if each is less than a number
+                min = each; // Update min if a smaller number is found
+            }
+        }
+        return min;
+    }
+
 }
+//Write a method that can find the Minimum number from an int Array
--- src/week7/ArraySortDesc_Sahar.java
+++ src/week7/ArraySortDesc_Sahar.java
@@ -1,4 +1,36 @@
 package week7;
 
+import java.util.Arrays;
+
 public class ArraySortDesc_Sahar {
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(sortDescending(new int[]{45, 14, 6, 95, 16, -19, 39})));
+
+    }
+
+    public static int[] sortDescending(int[] array) {
+
+        for (int i = 0; i < array.length; i++) { // first loop to iterate through
+            for (int j = 0; j < array.length; j++) { // start from the beginning and compare. add length-1 to not go out of bounds bc youre checking the number next to j
+                if (array[i] > array[j]) { // comparing j with j+1 which is the j right before
+                    int temp = array[j]; // temp varible to hold j
+                    array[j] = array[i];   // Swap array[j] and array[j+1]
+                   array[i] = temp;
+                }
+            }
+
+        }
+        return array;
+    }
+
 }
+
+/*
+Question3: Array - Sort Descending
+Write a return method that can sort an int array in descending order without using the sort method of the Arrays class
+Ex: int[] arr = {10,20,7, 8, 90};
+
+arr = Sort(arr); ==> {90, 20, 10, 8, 7};
+ */
