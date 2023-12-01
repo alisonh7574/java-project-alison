9c3a51adf1ed377a313cdb54fbf916b8a181330d
Saharnaz1 <sa_rdh@yahoo.com>
2023-11-30 23:06:08 -0500

    Add week 8 code

--- src/week8/MoveZerosToEnd_Sahar.java
+++ src/week8/MoveZerosToEnd_Sahar.java
@@ -1,4 +1,37 @@
 package week8;
 
+import java.util.Arrays;
+
 public class MoveZerosToEnd_Sahar {
+    public static void main(String[] args) {
+
+        int[] arr = {1,0,2,0,3,0,4,0};
+        System.out.println(Arrays.toString(moveZeros(arr)));
+
+    }
+
+    public static int[] moveZeros(int[] nums) {
+
+        int[] moved = new int[nums.length]; // make new array
+        int index = 0; // index of array 1
+
+
+        for (int each : nums) { //goes through the original array [5, 2, 1, 23, 0, 0, 0, 0]
+            if (each != 0){  // if the number is NOT zero
+                moved[index++]= each; // if the number is NOT zero it will get added to "moved" array
+
+            }
+        }
+
+        return moved;
+
+    }
+
+
 }
+    /*
+    Array - Move Zeros to the End
+    Write a method that can move all the zeros to last indexes of the array (Do Not Use Sort Method) Ex: input: {1,0,2,0,3,0,4,0}; output: [1, 2, 3, 4, 0, 0, 0, 0]
+*/
+
+
--- src/week8/SumtoZero_Sahar.java
+++ src/week8/SumtoZero_Sahar.java
@@ -1,4 +1,47 @@
 package week8;
 
+import java.util.Arrays;
+
 public class SumtoZero_Sahar {
+
+    public static void main(String[] args) {
+        int num = 4;
+        int[] result = sumToZero(num);
+        System.out.println(Arrays.toString(result));
+    }
+
+
+    public static int[] sumToZero(int num) {
+
+        int[] result;
+
+        if (num % 2 == 1) { // checks if number is odd
+            // For odd N, return an array with N-1 integers and 0
+            result = new int[num];
+
+            for (int i = -(num / 2); i <= num / 2; i++) {
+                if (i == 0) continue;
+                result[i + (num / 2) - 1] = i;
+            }
+        } else {
+            // For even N, return an array with N/2 positive and N/2 negative integers
+            result = new int[num];
+            for (int i = -(num / 2); i < num / 2; i++) {
+                result[i + (num / 2)] = i;
+            }
+        }
+
+        return result;
+    }
+
+
+
 }
+    /*
+    Array - N unique integers that sum up to 0
+    Write a function that given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0. The function can return any such array.
+    For example, given N = 4, the function could return [1,0, -3,2] or [-2,1, -4,5]. The answer [1, - 1,1,3] would be incorrect (because value 1 occurs twice). For N = 3 one of the possible answers is [-1,0,1] (but there are many more correct answers).
+    */
+
+
+
